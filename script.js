50a65b7867f69b34cddca7fd3d9d317b86ffa018
Vinicius Henrique <79711257+MrVihks@users.noreply.github.com>
2023-02-28 14:58:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let clicked = false; 
+document.addEventListener('click', (e)=>{
+    const el = e.target;
+    const menuBtn = document.querySelector('#menu');
+    const hiddenNav = document.querySelector('.hiddennav');
+
+
+    if(el === menuBtn){
+        clicked = !clicked ? clicked = true : clicked = false;
+        return !clicked ? hiddenNav.classList.add("showed") : hiddenNav.classList.remove("showed");
+    };
+});
